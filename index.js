0cae398c95164b2bde604798df10749c86682574
Razaq Mzale <rmmzale@outlook.com>
2023-01-09 11:23:14 +0300

    fixed anchors on buttons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hamTop = document.querySelector(".top");
 const hamMiddle = document.querySelector(".middle");
 const hamBottom = document.querySelector(".bottom");
 const mobileNav = document.querySelector(".mobile-nav-list");
+const learnBtn = document.querySelector(".learn-more");
 
 // Event Listeners
 
@@ -35,4 +36,15 @@ mobileNav.addEventListener("click", (e) => {
   hamMiddle.classList.toggle("unclicked");
   hamBottom.classList.toggle("clicked");
   hamBottom.classList.toggle("unclicked");
+});
+
+learnBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const id = e.target.getAttribute("href");
+  console.log(id);
+  if (!e.target.classList.contains("btn-link")) {
+    return;
+  }
+
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
 });
