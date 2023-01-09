f12f20869dc9c6b002c9ac5623a13cea1767521b
Razaq Mzale <rmmzale@outlook.com>
2023-01-09 10:46:17 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Selecting
+
+const hamMenu = document.querySelector(".hamburger-menu");
+const hamTop = document.querySelector(".top");
+const hamMiddle = document.querySelector(".middle");
+const hamBottom = document.querySelector(".bottom");
+const mobileNav = document.querySelector(".mobile-nav-list");
+
+// Event Listeners
+
+hamMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  hamTop.classList.toggle("clicked");
+  hamTop.classList.toggle("unclicked");
+  hamMiddle.classList.toggle("clicked");
+  hamMiddle.classList.toggle("unclicked");
+  hamBottom.classList.toggle("clicked");
+  hamBottom.classList.toggle("unclicked");
+  mobileNav.classList.toggle("inactive");
+});
+
+// Smooth Scrolling
+mobileNav.addEventListener("click", (e) => {
+  e.preventDefault();
+  const id = e.target.getAttribute("href");
+  console.log(id);
+  if (!e.target.classList.contains("mobile-nav-link")) {
+    return;
+  }
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  mobileNav.classList.toggle("inactive");
+  hamTop.classList.toggle("clicked");
+  hamTop.classList.toggle("unclicked");
+  hamMiddle.classList.toggle("clicked");
+  hamMiddle.classList.toggle("unclicked");
+  hamBottom.classList.toggle("clicked");
+  hamBottom.classList.toggle("unclicked");
+});
